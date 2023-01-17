611f6fe65bbf5d5d8f14bd41ec46560ece1cae63
pleasantvik <adedayo.ayoola5683@gmail.com>
2023-01-17 11:38:39 +0100

    signinpage

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-
+import loginImg from "resources/images/login.jpg";
 export const Signin = () => {
-  return <div>Signin</div>;
+  return (
+    <section className="max-w-6xl mx-auto">
+      <h1 className="text-3xl text-center mt-6 font-bold">Sign in</h1>
+      <div>
+        <div>
+          <img src={loginImg} alt="A cage under lock" />
+        </div>
+        <form>Form</form>
+      </div>
+    </section>
+  );
 };
