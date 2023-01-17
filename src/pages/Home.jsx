9cecc57f9a6ca30bc3e008761e9378696abad3eb
pleasantvik <adedayo.ayoola5683@gmail.com>
2023-01-17 11:12:02 +0100

    created the header component with its routing functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const Home = () => {
-  return <div>Home</div>;
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true);
+    }, 5000);
+  }, []);
+
+  return (
+    <div>
+      {!loading && <h1>Loading</h1>}
+      {loading && <h1>Home</h1>}
+    </div>
+  );
 };
