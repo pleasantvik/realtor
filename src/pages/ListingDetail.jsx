10f6db6237473c015193d1c15fa5a13a6591279f
pleasantvik <adedayo.ayoola5683@gmail.com>
2023-01-25 01:01:20 +0100

    feat: add share copied link functionality

--- src/pages/ListingDetail.jsx
+++ src/pages/ListingDetail.jsx
@@ -13,14 +13,27 @@ import SwiperCore, {
 } from "swiper";
 
 import "swiper/css/bundle";
+import { FaShare } from "react-icons/fa";
 
 export const ListingDetail = () => {
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
   const { type, listingId } = useParams();
   console.log(type, listingId);
 
   SwiperCore.use([Autoplay, Navigation, Pagination]);
+
+  const handleShareLinkCopied = () => {
+    navigator.clipboard.writeText(window.location.href);
+    setShareLinkCopied(true);
+    console.log("uncopied");
+
+    setTimeout(() => {
+      setShareLinkCopied(false);
+      console.log("copied");
+    }, 2000);
+  };
 
   useEffect(() => {
     const fetchListing = async () => {
@@ -40,7 +53,7 @@ export const ListingDetail = () => {
     return <Spinner />;
   }
   return (
-    <main>
+    <main className="relative">
       <Swiper
         slidesPerView={1}
         navigation
@@ -61,6 +74,17 @@ export const ListingDetail = () => {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        onClick={handleShareLinkCopied}
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+      >
+        <FaShare className="text-lg text-slate-500 " />
+      </div>
+      {shareLinkCopied && (
+        <p className="fixed top-[20%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">
+          Link copied
+        </p>
+      )}
     </main>
   );
 };
